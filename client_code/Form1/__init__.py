5b98a53a4c7ab78af1c0481e54ecf24b0ee622f9
hans.melberg@gmail.com <hans.melberg@gmail.com>
2020-05-05 20:10:37 +0000

    Saved automatically

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,10 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Form1(Form1Template):
 
@@ -8,5 +13,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def email_pressed_enter(self, **event_args):
+    path = 'https://scented-perfect-raccoon.anvil.app/_/api/'
+    email=self.email.text
+    self.link.url = path+f'_/api/send/{email}'
+
 
 
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,21 @@
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.email
+import anvil.server
+
+import anvil.server
+
+
+@anvil.server.http_endpoint("/send/:email")
+def send_url(email):
+  #ip = anvil.server.request.remote_address
+  url=anvil.server.request.origin
+  anvil.email.send(from_name="send2me", 
+                 to=email, 
+                 subject="Link",
+                 text=url)
+
+
+

0bbd71e179a1b47f0cd16994c566868673f32754
hans.melberg@gmail.com <hans.melberg@gmail.com>
2021-09-07 23:39:28 +0000

    print just header

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -11,13 +11,13 @@ import anvil.server
 @anvil.server.http_endpoint("/send/:email")
 def send_url(email):
   #ip = anvil.server.request.remote_address
-  url=anvil.server.request.origin
-  body=anvil.server.request.body_json
+  #url=anvil.server.request.origin
+  #body=anvil.server.request.body_json
   headers=anvil.server.request.headers
   #email='hans.melberg@gmail.com'
-  print('url',url)
+  #print('url',url)
   #print('ip',ip)
-  print('body',body)
+  #print('body',body)
   print('headers', headers)
   url=headers['referer']
   

b2ab90076d56020211c06c8cc6436ba93a40ba73
hans.melberg@gmail.com <hans.melberg@gmail.com>
2021-09-07 23:33:29 +0000

    trying to get full url

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -11,14 +11,14 @@ import anvil.server
 @anvil.server.http_endpoint("/send/:email")
 def send_url(email):
   #ip = anvil.server.request.remote_address
-  #url=anvil.server.request.origin
-  #body=anvil.server.request.body_json
+  url=anvil.server.request.origin
+  body=anvil.server.request.body_json
   headers=anvil.server.request.headers
   #email='hans.melberg@gmail.com'
-  #print('url',url)
+  print('url',url)
   #print('ip',ip)
-  #print('body',body)
-  #print('headers', headers)
+  print('body',body)
+  print('headers', headers)
   url=headers['referer']
   
   anvil.email.send(from_name="send2me", 
